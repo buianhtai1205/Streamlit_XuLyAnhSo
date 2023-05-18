9ed1141002fc8398278d41d60ceec6141c6b9ad5
buianhtai1205 <buianhtai2017tq@gmail.com>
2023-05-18 21:10:53 +0700

    fix bug

--- NhanDangKhuonMat/Buoc3/predict.py
+++ NhanDangKhuonMat/Buoc3/predict.py
@@ -59,7 +59,7 @@ def runPredict():
 
     tm = cv.TickMeter()
 
-    cap = cv.VideoCapture(2)
+    cap = cv.VideoCapture(0)
 
     frameWidth = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
     frameHeight = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
